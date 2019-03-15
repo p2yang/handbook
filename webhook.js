20259196f82268629f4a7fcc1aac7259c3104e70
Sym <p2yang@163.com>
2019-03-15 22:48:00 +0800

    fix

--- webhook.js
+++ webhook.js
@@ -28,6 +28,6 @@ handler.on('push', function (event) {
 function runCommand( cmd, args, callback ){
     var child = spawn( cmd, args );
     var response = '';
-    child.stdout.on('data', function( buffer ){ resp += buffer.toString(); });
-    child.stdout.on('end', function(){ callback( resp ) });
+    child.stdout.on('data', function( buffer ){ response += buffer.toString(); });
+    child.stdout.on('end', function(){ callback( response ) });
 }
