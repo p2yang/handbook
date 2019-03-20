2e0aa1cdcae858a7bc109ddd4ec60c9caad0e4e5
Sym <p2yang@163.com>
2019-03-20 10:48:54 +0800

    refactor: 调整文档位置

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+# 确保脚本抛出遇到的错误
+set -e
+
 SITE_PATH='/www/learning-handbook'
 USER='admin'
 USERGROUP='admin'
@@ -9,3 +12,12 @@ git reset --hard origin/master
 git clean -f
 git pull
 chown -R $USER:$USERGROUP $SITE_PATH
+
+# 生成静态文件
+npm run build
+
+git add -A
+git commit -m 'deploy'
+git push -f
+
+cd -
