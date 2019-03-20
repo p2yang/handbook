e10473f5804e9c9b966fb997d255270484bbe943
Sym <p2yang@163.com>
2019-03-20 10:58:56 +0800

    fix: fix sh

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,17 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-SITE_PATH='/www/learning-handbook'
-USER='admin'
-USERGROUP='admin'
-
-cd $SITE_PATH
+cd /www/learning-handbook
 
 git reset --hard origin/master
 git clean -f
 git pull
-chown -R $USER:$USERGROUP $SITE_PATH
 
 # 生成静态文件
 npm run build
 
 git add -A
 git commit -m 'deploy'
-git push -f
+git push
 
 cd -
