627bf4e6269f010d9f4e49ac91674c27b81658cb
p2yang <p2yang@163.com>
2020-02-09 13:49:32 +0800

    use CNAME

---
+++ deploy-copy.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# 确保脚本抛出遇到的错误
+set -e
+
+cd /www/learning-handbook
+
+git reset --hard origin/master
+git clean -f
+git pull
+
+# 生成静态文件
+npm run build
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,25 @@
-#! /bin/bash
+#!/usr/bin/env sh
+
 # 确保脚本抛出遇到的错误
 set -e
 
-cd /www/learning-handbook
-
-git reset --hard origin/master
-git clean -f
-git pull
-
 # 生成静态文件
-npm run build
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+echo 'handbook.p2yang.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:p2yang/learning-handbook.git master:gh-pages
+
+cd -
