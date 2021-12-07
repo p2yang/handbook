1d53f9830a7c3a13f0706199d8a21415a20b62cc
Sym <p2yang@163.com>
2021-12-07 19:25:16 +0800

    update style

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,6 +13,7 @@ module.exports = {
   base: '/handbook/',
   dest: 'dist',
   themeConfig: {
+    home: '/guide/',
   	navbar: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
